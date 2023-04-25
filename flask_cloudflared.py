ee5201570ace4661af725b9f8ff7ac93a1293a1d
mnt4 <58336655+mnt4@users.noreply.github.com>
2023-04-25 09:48:14 +0200

    Fixed a NameError + backdated release

--- flask_cloudflared.py
+++ flask_cloudflared.py
@@ -15,39 +15,39 @@ from pathlib import Path
 CLOUDFLARED_CONFIG = {
     ('Windows', 'AMD64'): {
         'command': 'cloudflared-windows-amd64.exe',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-windows-amd64.exe'
     },
     ('Windows', 'x86'): {
         'command': 'cloudflared-windows-386.exe',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-386.exe'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-windows-386.exe'
     },
     ('Linux', 'x86_64'): {
         'command': 'cloudflared-linux-amd64',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-linux-amd64'
     },
     ('Linux', 'i386'): {
         'command': 'cloudflared-linux-386',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-linux-386'
     },
     ('Linux', 'arm'): {
         'command': 'cloudflared-linux-arm',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-linux-arm'
     },
     ('Linux', 'arm64'): {
         'command': 'cloudflared-linux-arm64',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-linux-arm64'
     },
     ('Linux', 'aarch64'): {
         'command': 'cloudflared-linux-arm64',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-linux-arm64'
     },
     ('Darwin', 'x86_64'): {
         'command': 'cloudflared',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-darwin-amd64.tgz'
     },
     ('Darwin', 'arm64'): {
         'command': 'cloudflared',
-        'url': 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-darwin-amd64.tgz'
+        'url': 'https://github.com/cloudflare/cloudflared/releases/tag/2023.4.1/download/cloudflared-darwin-amd64.tgz'
     }
 }
 
@@ -69,7 +69,7 @@ def _extract_tarball(tar_path, filename):
     for item in tar:
         tar.extract(item, tar_path)
         if item.name.find(".tgz") != -1 or item.name.find(".tar") != -1:
-            extract(item.name, "./" + item.name[:item.name.rfind('/')])
+            tar.extract(item.name, "./" + item.name[:item.name.rfind('/')])
 
 def _download_cloudflared(cloudflared_path, command):
     system, machine = platform.system(), platform.machine()
